055ebd598a741fda1b787bda49bd3729efb4c8e6
Karim <karimbasharov@gmail.com>
2020-03-25 13:12:00 +0200

    Змейка

--- SnakeKarim/HorizonallLine.cs
+++ SnakeKarim/HorizonallLine.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace SnakeKarim
+namespace Snake
 {
-    class HorizonallLine : Figure
-    {
-        public HorizonallLine(int xLeft, int xRight, int y, char sym)
-        {
-            pList = new List<Point>();
-            for (int x = xLeft; x <= xRight; x++)
-            {
-                Point p = new Point(x, y, sym);
-                pList.Add(p);
-            }
-        }
-        
-    }
+	class HorizontalLine : Figure
+	{
+		public HorizontalLine(int xLeft, int xRight, int y, char sym)
+		{
+			pList = new List<Point>();
+			for (int x = xLeft; x <= xRight; x++)
+			{
+				Point p = new Point(x, y, sym);
+				pList.Add(p);
+			}
+		}
+	}
 }
